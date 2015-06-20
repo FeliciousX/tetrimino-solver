6a2ccab7e576d36877d903a2718e8ed1edb3b198
feliciousx <feliciousx@gmail.com>
2015-06-20 12:53:53 +0800

    Uses a block item and enum tetrimino shapes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+enum Tetromino {
+    I,
+    O,
+    T,
+    J,
+    L,
+    S,
+    Z,
+}
+
+struct Block {
+    shape: Tetromino,
+}
+
+impl Block {
+    fn new(tetris: Tetromino) -> Block {
+        Block {
+            shape: tetris,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let x = Block::new(Tetromino::T);
+
+    match x.shape {
+        Tetromino::I => println!("I shape"),
+        Tetromino::O => println!("O shape"),
+        Tetromino::T => println!("T shape"),
+        Tetromino::J => println!("J shape"),
+        Tetromino::L => println!("L shape"),
+        Tetromino::S => println!("S shape"),
+        Tetromino::Z => println!("Z shape"),
+    }
 }
