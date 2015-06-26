e1e98b2ca85bb8c01fb95d4805de63571362aecf
feliciousx <feliciousx@gmail.com>
2015-06-26 10:51:53 +0800

    Writes documentation for board/mod.rs

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -12,6 +12,17 @@ pub struct Board {
 }
 
 impl Board {
+    /// Construct a new `Board` object.
+    ///
+    /// Accepts `height: i32` and `width: i32` as parameters to construct
+    /// a board object with size `height x width`.
+    ///
+    /// # Examples
+    /// ```
+    /// use board::Board;
+    ///
+    /// let board = Board::new(5, 10);
+    /// ```
     pub fn new(height: i32, width: i32) -> Board {
         let mut squares: Vec<Vec<Square>> = Vec::new();
         for _ in 0..height {
@@ -32,22 +43,70 @@ impl Board {
         }
     }
 
+    /// Gets `height` of board
+    ///
+    /// # Examples
+    /// ```
+    /// use board::Board;
+    ///
+    /// let board = Board::new(6, 12);
+    /// height = board.get_height();
+    ///
+    /// assert_eq!(height, 6);
+    /// ```
     pub fn get_height(&self) -> i32 {
         self.height
     }
 
+    /// Gets `width` of board
+    ///
+    /// # Examples
+    /// ```
+    /// use board::Board;
+    ///
+    /// let board = Board::new(6, 12);
+    /// width = board.get_width();
+    ///
+    /// assert_eq!(width, 12);
+    /// ```
     pub fn get_width(&self) -> i32 {
         self.width
     }
 
+    /// Check if `Board` has been completely filled perfectly.
+    ///
+    /// # Examples
+    /// ```
+    /// use board::Board;
+    ///
+    /// let board = Board::new(5, 10);
+    ///
+    /// assert_eq!(false, board.is_complete());
+    /// ```
     pub fn is_complete(&self) -> bool {
         self.complete
     }
 
+    /// Set `Board` completion status.
+    ///
+    /// # Examples
+    /// ```
+    /// use board::Board;
+    ///
+    /// let board = Board::new(4, 5);
+    ///
+    /// board.set_complete(true);
+    ///
+    /// assert_eq!(true, board.is_complete());
+    /// ```
     pub fn set_complete(&mut self, status: bool) {
         self.complete = status;
     }
 
+    /// Place a `Block` on the `Board`.
+    ///
+    /// The `Block` will be placed according to the `Block.anchor`
+    ///
     pub fn fill(&mut self, block: &block::Block) {
         // TODO: add block to the coordinate using the anchor coordinate as guide
         println!("filling");
